11ab14c1619c20d4fc0a88c5a3033c28be0a4b83
taylo127 <49724279+taylo127@users.noreply.github.com>
2019-04-19 09:37:56 -0500

    Update Source.cpp

---
+++ Project 2 - Morse Code Decoder/Project 2 - Morse Code Decoder/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Morse_code_Class.h"
+#include <fstream>
+
+using namespace std;
+
+void main() {
+  ifstream fin("morse.txt");
+  while (fin.good) {
+    
+  }
+  
+}
